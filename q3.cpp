04e4a40a6472d41a51fc616d447896b51fcbb621
20250458 권지원 <jiwonjjang0403@gmail.com>
2026-04-23 20:31:15 +0900

    Add files via upload

---
+++ q3.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void echo(int arr[x]){
+	for(int i = 0; i < x; i++){
+		scanf("%d", &arr[i]);
+	}
+}
+
+void input(char arr2[y]){
+}
+
+
+int main(){
+	int n;
+	
+	printf("");
+	scanf("%d\n", &n);
+	echo(arr[x]);
+	
+	
+	
+	return 0;
+}
